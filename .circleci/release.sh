19f924f694f8490bae9f12f223318d1cdd0dde87
Sheikh, Haroon <haroon.sheikh@sky.uk>
2019-07-04 15:30:54 +0100

    Wait for build to be successful before deploying

--- .circleci/release.sh
+++ .circleci/release.sh
@@ -5,12 +5,6 @@ if [ -z "${GITHUB_TOKEN}" ]; then
   exit 1
 fi
 
-${GOPATH}/bin/github-release release \
-    -u ${CIRCLE_PROJECT_USERNAME} \
-    -r ${CIRCLE_PROJECT_REPONAME} \
-    --draft -t ${CIRCLE_TAG} \
-    -n "${CIRCLE_TAG}"
-
 cd deploy/
 for i in `ls`; do
     ${GOPATH}/bin/github-release upload \
@@ -22,10 +16,3 @@ for i in `ls`; do
         exit 1
     fi
 done
-
-# Publish Release
-${GOPATH}/bin/github-release edit \
-    -u ${CIRCLE_PROJECT_USERNAME} \
-    -r ${CIRCLE_PROJECT_REPONAME} \
-    -t ${CIRCLE_TAG} \
-    -n "${CIRCLE_TAG}"
